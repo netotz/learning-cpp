281e6338da5f3338de852e16afe92dddc8f2225b
netotz <neto.otz@hotmail.com>
2020-01-25 12:33:13 -0600

    Wrap switch's cases in explicit blocks

--- hackerrank/box_it.cpp
+++ hackerrank/box_it.cpp
@@ -63,17 +63,19 @@ void check2() {
 		int type;
 		std::cin >> type;
         switch (type) {
-            case 1:
+            case 1:{
                 std::cout << temp << std::endl;
                 break;
-            case 2:
+            }
+            case 2:{
                 int l, b, h;
                 std::cin >> l >> b >> h;
                 Box NewBox(l, b, h) ;
                 temp = NewBox;
                 std::cout << temp << std::endl;
                 break;
-            case 3:
+            }
+            case 3:{
                 int l, b, h;
                 std::cin >> l >> b >> h;
                 Box NewBox(l, b, h) ;
@@ -84,13 +86,16 @@ void check2() {
                     std::cout << "Greater\n";
                 }
                 break;
-            case 4:
+            }
+            case 4:{
                 std::cout << temp.CalculateVolume()  << std::endl;
                 break;
-            case 5:
+            }
+            case 5:{
                 Box NewBox(temp) ;
                 std::cout << NewBox << std::endl;
                 break;
+            }
         }
 	}
 }
